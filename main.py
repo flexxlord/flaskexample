b09745a20614098fbf8d6425c1491a90c75d821a
flexxlord <66664053+flexxlord@users.noreply.github.com>
2020-07-23 18:42:50 -0400

    trying

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+app = Flask(__name__, static_url_path='', static_folder='')
+
+@app.route('/')
+def root():
+    return app.send_static_file('name.html')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
+
+print("hi")
